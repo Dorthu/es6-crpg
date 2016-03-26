afe8f8509e2b70479fd8e65ebb1bb1ea43c4fc99
Dorthu <dorthu2@gmail.com>
2016-03-26 15:37:41 -0400

    Started implementing A*

--- src/path_network.js
+++ src/path_network.js
@@ -1,3 +1,12 @@
+const dirs = [ {x:1, z:0}, {x:0, z: 1}, {x:-1, z:0}, {x:0, z:-1} ];
+
+class PathNode {
+    constructor(x, z) {
+        this.x = x;
+        this.z = z;
+        this.neighbors = [];
+    }
+}
 
 class PathNetwork {
     constructor(grid) {
@@ -8,6 +17,7 @@ class PathNetwork {
     }
 
     rebuild_network(hard=false) {
+        /////TODO - maybe do this, maybe not?
         /*
             Generates a new PathNetwork based on the grid.  Should be called
             to rebuild the network if more movable spaces open up that are important
@@ -32,22 +42,45 @@ class PathNetwork {
         if(!this.first_movable_space) { return; }
 
         ///recursively walk the path and find all spaces we can reach
-
+        
     }
 
-    path_to_point(point) {
+    path_to_point(graph, start, goal) {
         /*
-            Use the generated network to find the best route to a given point.
+            This is an implementation of A* based on
+            http://www.redblobgames.com/pathfinding/a-star/implementation.html
+            Super special thanks to those guys!
 
             Takes: point - { x: int, y: int }
             Returns: an array of points (as above), null if point is unreachable
         */
-        if(!this.network) {
+/*        if(!this.network) {
             console.log("WARNING: PathNetwork was never built (or no movable spaces in grid)!");
             return null;
         }
+*/
+        let frontier = [];
+        frontier.push(start);
+        came_from = {};
+        cost_so_far = {};
+        came_from[start.x+';'+start.z] = null;
+        cost_so_far[start.x+';'+start.z = 0;
+        
+        while(frontier.length) {
+            let cur = frontier.shift();
+        }
     }
 
+    _get_neighbors(x, z) {
+        let ret = [];
+        for(let d of dirs) {
+            if(this.grid.can_move_to(x+d.x, z+d.z)) {
+                ret.push({x: x+d.x, z: z+d.z});
+            }
+        }
+        return ret;
+    }
+    
     step_to_point(point) {
         /*
             A convenience method, contains the next space to move to to reach a given
