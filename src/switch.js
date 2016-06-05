fb9719ed32f603643b569336c1baa2be859d26c4
Dorthu <dorthu2@gmail.com>
2016-06-05 15:05:55 -0400

    Switch once

--- src/switch.js
+++ src/switch.js
@@ -5,12 +5,24 @@ class Switch extends GridObject {
         super(grid, loc, mats, desc, extra);
 
         this.event = extra['event'];
+        this.once = extra['once'];
+        this.key = extra['key'];
 
         this.useable = true;
+
+        if(this.once) {
+            if(this.grid.level.get_value(this.key) !== true) {
+                this.useable = false;
+            }
+        }
     }
 
     use(player) {
         this.grid.event_manager.dispatchArbitrary(this.event);
+        if(this.once) {
+            this.useable = false;
+            this.grid.level.set_value(this.key, false);
+        }
     }
 }
 
